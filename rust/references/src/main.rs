756e924fb1c4037b2a7245b4ddc87aaa931a1f81
windingtheropes <windingtheropes@gmail.com>
2022-04-14 22:46:55 -0400

    add refs

--- rust/references/src/main.rs
+++ rust/references/src/main.rs
@@ -6,6 +6,11 @@ fn main() {
      let trimmed = takes_a_ref(&s);
      println!("Here is s after running the function: {} and here is trimmed {}", s, trimmed);
 
+     let mut s = String::from("Not Updated");
+     println!("Here is the non updated string, {}", s);
+     takes_ref_2(&mut s);
+     println!("Inside main scope.");
+     println!("Here is s inside main scope, after running secondary function with ref: {}", s);
      
 }
 
@@ -15,4 +20,12 @@ fn takes_a_ref(s: &String) -> String {
     println!("This does not have to return the original value, and the string can continue to be used in the main scope.");
     println!("Here is the string inside another scope, the trimming function: {}",s);
     s.trim().to_string()
+} // though s goes out of scope here, that doesn't matter because s is a reference and this function doesn't own the actual variable
+
+// references like &ref are immutable by default, their value can't be changed.
+
+fn takes_ref_2(s: &mut String)
+{
+    println!("Inside secondary function scope");
+    s.push_str(", This is an update.");
 }
